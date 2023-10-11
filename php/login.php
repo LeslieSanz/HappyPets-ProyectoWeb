2bb342f9ed1a905ac32464a7c00dd06c83395530
Anthont23 <130714369+Anthont23@users.noreply.github.com>
2023-10-11 09:15:40 -0500

    Never again

---
+++ php/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login-HappyPets</title>
+        <link rel="stylesheet" href="../css/styleLogin.css"> 
+        <link rel="icon" href = "../img/logos/logopets.png" type = "image/x-icon">
+    </head>
+    <body>
+        <main>
+        <div class="box-log">
+            <span class="borderLine"></span>
+            <form method="post" action="">
+                <h2>Iniciar sesión</h2>
+                <?php
+                include("dbconfig.php");
+                include("controlador_usuario.php")
+                ?>
+                <div class="inputBox">
+                    <input id="usuario" type="text" required="required" name="usuario">
+                    <span>Usuario</span>
+                    <i></i>
+                </div>
+                <div class="inputBox">
+                    <input id="password" type="password" required="required" name="password">
+                    <span>Contraseña</span>
+                    <i></i>
+                </div>
+                <div class="ver">
+                    <div class="fas fa-eye verPassword" onclick="vista()" id="verPassword"></div>
+                </div>
+                <div class="links">
+                    <a href="#">Recordar contraseña</a>
+                    <a href="../html/registro.html">Registrarse</a>
+                </div>
+                <input name="btningresar" class="btn" type="submit" value="Login">
+            </form>
+        </div>
+        </main>
+    </body>
+</html>
