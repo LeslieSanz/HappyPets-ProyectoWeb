e22cafb013c18977c347e90c10cdc44f4de03144
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2023-10-24 21:41:16 -0500

    guardar datos en reporte

--- app/modelo/Usuario.php
+++ app/modelo/Usuario.php
@@ -62,6 +62,23 @@ class Usuario {
     
         return $usuarios;
     }
+
+    public function buscarUsuario($id) {
+        $usuario = null;
+    
+        // Preparar la consulta SQL con un parámetro
+        $stmt = $this->conn->prepare("SELECT * FROM usuario WHERE id = ?");
+        $stmt->bind_param("i", $idUsuario); // "i" indica que el parámetro es un entero
+        $stmt->execute();
+        $result = $stmt->get_result();
+    
+        if ($result->num_rows > 0) {
+            $usuario = $result->fetch_assoc();
+        }
+    
+        return $usuario;
+    }
+    
     
 }
 ?>
--- app/vista/index_log.php
+++ app/vista/index_log.php
@@ -1,4 +1,3 @@
 <?php
 session_start();
-// var_dump($_SESSION); de prueba para ver si funciona
 echo 'Bienvenido, ' . $_SESSION['usuario'];
