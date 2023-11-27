b0383dd69e7a78fc13c6cef512737a445c3c93be
LeslieSanz <114875034+LeslieSanz@users.noreply.github.com>
2023-11-26 19:26:59 -0500

    acomodando pop up de Registrar animal en el Admin

--- app/vista/admin/adminAnimales.php
+++ app/vista/admin/adminAnimales.php
@@ -94,55 +94,72 @@ require_once __DIR__ . '/../../controlador/animalControlador.php';
     </section>
     
     <!--POPUP Agregar animal-->
-    <section class="modal ">
+    <section class="modal" id="miModal">
+
         <div class="modal_container">
-        <h1>Registro de animales en adopción</h1>
+        
+        <div class="modal-header">
+            <h1>Registro de animales en adopción</h1>
+            <a href="#" class="modal_close">X</a>
+        </div>
+
+        
         <form action="../../controlador/animalControlador.php" method="post" enctype="multipart/form-data">
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required><br><br>
+            <div class="form-container">
 
-            <label for="especie">Especie:</label>
-            <select id="especie" name="especie" required>
-                <option value="Felino">Felino</option>
-                <option value="Canino">Canino</option>
-            </select><br><br>
+                    <div class="form-column">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" id="nombre" name="nombre" required><br><br>
 
-            <label for="sexo">Sexo:</label>
-            <select id="sexo" name="sexo" required>
-                <option value="Hembra">Hembra</option>
-                <option value="Macho">Macho</option>
-            </select><br><br>
+                        <label for="especie">Especie:</label>
+                        <select id="especie" name="especie" required>
+                            <option value="Felino">Felino</option>
+                            <option value="Canino">Canino</option>
+                        </select><br><br>
 
-            <label for="edad">Edad:</label>
-            <select id="edad" name="edad" required>
-                <option value="0-6 meses">0 a 6 meses</option>
-                <option value="6-12 meses">6 a 12 meses</option>
-                <option value="1-2 años">1 a 2 años</option>
-                <option value="2-4 años">2 a 4 años</option>
-                <option value="Más de 4 años">Más de 4 años</option>
-            </select><br><br>
+                        <label for="sexo">Sexo:</label>
+                        <select id="sexo" name="sexo" required>
+                            <option value="Hembra">Hembra</option>
+                            <option value="Macho">Macho</option>
+                        </select><br><br>
 
-            <label for="tamano">Tamaño:</label>
-            <select id="tamano" name="tamano" required>
-                <option value="pequeño">Pequeño</option>
-                <option value="mediano">Mediano</option>
-                <option value="grande">Grande</option>
-            </select><br><br>
+                        <label for="edad">Edad:</label>
+                        <select id="edad" name="edad" required>
+                            <option value="0-6 meses">0 a 6 meses</option>
+                            <option value="6-12 meses">6 a 12 meses</option>
+                            <option value="1-2 años">1 a 2 años</option>
+                            <option value="2-4 años">2 a 4 años</option>
+                            <option value="Más de 4 años">Más de 4 años</option>
+                        </select><br><br>
 
-            <label for="caracteristicas">Características:</label><br>
-            <textarea id="caracteristicas" name="caracteristicas" rows="4" cols="50" required></textarea><br><br>
+                        <label for="tamano">Tamaño:</label>
+                        <select id="tamano" name="tamano" required>
+                            <option value="pequeño">Pequeño</option>
+                            <option value="mediano">Mediano</option>
+                            <option value="grande">Grande</option>
+                        </select><br><br>
+                    </div>
+                    
+                    <div class="form-column">
+                        <label for="caracteristicas">Características:</label><br>
+                        <textarea id="caracteristicas" name="caracteristicas" rows="4" cols="50" required></textarea><br><br>
 
-            <label for="razon">Razón:</label><br>
-            <textarea id="razon" name="razon" rows="4" cols="50" required></textarea><br><br>
+                        <label for="razon">Razón:</label><br>
+                        <textarea id="razon" name="razon" rows="4" cols="50" required></textarea><br><br>
 
-            <label for="foto">Subir Foto:</label>
-            <input type="file" id="foto" name="foto" accept="image/*" required><br><br>
+                        <label for="foto">Subir Foto:</label>
+                        <input type="file" id="foto" name="foto" accept="image/*" required><br><br>
 
-            <input type="submit" value="Ingresar" name="RegistrarAnimal">
-        </form>
-            
+                        <input type="submit" value="Ingresar" name="RegistrarAnimal">
+                    </div>
+                 </div>
+            </form> 
         </div>
     </section>
+
+    
+    
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="script.js"></script>
