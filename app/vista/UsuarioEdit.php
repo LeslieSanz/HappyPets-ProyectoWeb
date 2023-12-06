2bb79d100b2db2df4ea3d16d3fd188d3b96f190e
Anthont23 <130714369+Anthont23@users.noreply.github.com>
2023-12-06 03:10:33 -0500

    Update UsuarioEdit.php

--- app/vista/UsuarioEdit.php
+++ app/vista/UsuarioEdit.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Happy Pets</title>
-    <link rel="stylesheet" href="../../css/styleContacto.css" />
+    <link rel="stylesheet" href="../../css/styleEditUs.css" />
     <link rel="stylesheet"   href="../../css/header.css" /> 
     <link rel="stylesheet"   href="../../css/footer.css" />
-    <script src="../../js/adopta.js"></script>
     <link rel="icon" href = "../../img/logos/logopets.png" type = "image/x-icon">
     <script src="https://kit.fontawesome.com/09bca36bc3.js" crossorigin="anonymous"></script>
 </head>
@@ -45,82 +44,44 @@
         </ul>
     </nav>
 </header>
-    <!-- Enlance que vincula con el js-->
-    <script src="../../js/reporta.js"></script>
-    
-    <main>
-    
-    <div class="container">
-      <div class="contact-form">
-        <?php
-            if (isset($_SESSION["usuario"])) {
-                echo '<h2>Bienvenido '.$_SESSION["usuario"].'</h2>';
-                echo '<form action="../controlador/ActualizarUsuario.php" method="post">';
-                echo '<h3>
-                En este pequeño espacio podrás modificar todas tus credenciales
-              </h3>';
-              echo '<p>
-            <label for="nombre">Usuario</label>
-            <input type="text" name="nombre" class="respuesta" placeholder="'.$_SESSION["usuario"].'" /></p>
-          <p>
-            <label for="email">Correo electronico</label>
-            <input type="email" name="email" class="respuesta" placeholder="'.$_SESSION["email"].'" readonly = "readonly"/>
-          </p>
-          <p>
-            <label for="celular">Número de celular</label>
-            <input
-              type="tel"
-              name="celular"
-              class="respuesta"
-              pattern="[0-9]{3}[0-9]{3}[0-9]{3}"
-              placeholder="'.$_SESSION["celular"].'"
-              required
-            />
-          </p>
-          <p>
-            <label for="DNI">DNI</label>
-            <input type="text" name="DNI" class="respuesta" placeholder="'.$_SESSION["dni"].'"/>
-          </p>
-          <p>
-            <label for="Distrito">Distrito</label>
-            <input type="text" name="Distrito" class="respuesta" placeholder="'.$_SESSION["distrito"].'"/>
-          </p>
-          <p>
-            <label for="edad">Edad</label>
-            <input type="text" name="edad" class="respuesta" placeholder="0"/>
-          </p>
-          <p>';
-            }
+<main>
+<h1 class="title"> Actualizar Perfil </h1>
+<section class="update-profile-container">
+   <form action="" method="post" enctype="multipart/form-data">
+      <img src="../../uploads/5.jpg" alt="">
+      <div class="flex">
+         <div class="inputBox">
+            <?php
+            if (isset($_SESSION["usuario"]))
+            echo '<span>Usuario : </span>';
+            echo '<input type="text" name="name" required class="box" placeholder="Ingresar tu nombre" value="'. $_SESSION["usuario"] .'">';
+            echo '<span>Email : </span>';
+            echo '<input type="email" name="email" required class="box" placeholder="Ingresar tu email" value="'. $_SESSION['email'] .'">';
+            echo '<span>Foto de perfil : </span>';
+            echo '<input type="hidden" name="old_image" value="">';
+            echo '<input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png">';
             ?>
-          <!--el id se usará para el javascript-->
-                        <div class="container-fotos">
-                            <div class="form-foto">
-                                <input type="file" id="boton-file-usuario" 
-                                    name="foto-animal"
-                                    accept="image/*" onchange="mostrarImagen('usuario')"
-                                    required>
-
-                                <input type="button" value="Subir Foto del animal" class="form-file"
-                                        onclick="document.getElementById('boton-file-usuario').click()">
-                            </div>
-                            <div id="previsualizacion">
-                                <p>Imagen previa:</p>
-                                <img id="imagen-previa-usuario" src="../../img/reporta-img/gato-gif.gif"
-                                            alt="Vista previa de la imagen" required>
-                                <p id="nombre-archivo-usuario">Nombre del archivo: <span></span></p>
-                            </div>
-                        </div>
-
-
-            <button type="submit" 
-                    name= "enviarDatosForm"
-                    class="submit">Enviar</button>
-          </p>
-
-        </form>
-        
+         </div>
+         <div class="inputBox">
+         <?php
+            if (isset($_SESSION["usuario"]))
+            echo'<input type="hidden" name="old_pass" value="'. $_SESSION['password'] .'">';
+            echo'<span>Antigua contraseña :</span>';
+            echo'<input type="password" class="box" name="previous_pass" placeholder="'. $_SESSION['password'] .'" >';
+            echo'<span>Nueva contraseña :</span>';
+            echo'<input type="password" class="box" name="new_pass" placeholder="Ingresar nueva contraseña" >';
+            echo'<span>Confirmar contraseña :</span>';
+            echo'<input type="password" class="box" name="confirm_pass" placeholder="Confirmar nueva contraseña" >';
+            ?>
+         </div>
       </div>
-    </div>
+      <div class="flex-btn">
+         <input type="submit" value="Actualizar" name="update" class="btn">
+         <a href="admin_page.php" class="option-btn">Retroceder</a>
+      </div>
+   </form>
+
+</section>
 
     </main>
     <!--PIE DE PAGINA-->
